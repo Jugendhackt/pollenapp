48e6ca502f44d754ce98672314e265b80dbf8991
paalwi <paulawiesner@gmx.net>
2016-06-12 11:06:17 +0200

    Java Verbesserungen

---
+++ javacode/Baumabstand.java
@@ -0,0 +1,43 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Baumabstand {
+public static double l;
+public static double b;
+public static double h = 450; //VERAENDERN!!
+public static double w = 600;
+public static int x;
+public static int y;
+public static PrintWriter karte;
+
+	public static void main(String[] args) throws IOException {
+	
+		 BufferedReader in = new BufferedReader(new FileReader("test2.txt"));
+		 //dokument l,b
+		
+karte = new PrintWriter(new FileWriter ("karte"));
+     String line1 = in.readLine();
+     String line2 = in.readLine();
+   
+
+
+		while(line2 != null)	{
+			  l = Double.parseDouble(line1);
+     b = Double.parseDouble(line2);
+		
+		x = (int)((b * h / 180.0) + (h / 2));
+				y = (int)((l * w / 360.0) + (w / 2));
+				
+				karte.println(x);
+				karte.println(y);
+			line1 = in.readLine();
+			line2 = in.readLine();
+		}
+		karte.close();
+	}
+}
